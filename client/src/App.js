8bb1ba60c23620bb3fd9e7f8c58973e4be73ded9
JUHI1404 <juhimehta1404@gmail.com>
2024-05-01 00:03:48 +0530

    Navbar layout ready

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button, Navbar  } from "flowbite-react";
+import { NavbarComponent } from "./components/NavbarComponent";
+
+function App() {
+  return (
+    <NavbarComponent />
+  );
+}
+
+
+
+export default App;
