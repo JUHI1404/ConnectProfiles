2ba0ba9bff0f3b4d689cb4afef9346cdc4553140
JUHI1404 <juhimehta1404@gmail.com>
2024-04-30 22:15:13 +0530

    user routes and controllers addded

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,51 @@
+import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get users!" });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get user!" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const tokenUserId = req.userId;
+    const { avatar, ...inputs } = req.body;
+  
+    if (id !== tokenUserId) {
+      return res.status(403).json({ message: "Not Authorized!" });
+    }
+  
+    try {
+      const updatedUser = await prisma.user.update({
+        where: { id },
+        data: {
+          ...inputs,
+          ...(avatar && { avatar }),
+        },
+      });
+  
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to update users!" });
+    }
+  };
+  
